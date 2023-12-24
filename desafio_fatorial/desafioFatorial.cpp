31d4643f269a039a4a18b20dc2c63d053002dfcd
Bruno Lopes <121700662+BrLopes3@users.noreply.github.com>
2023-12-24 14:35:41 -0500

    updates

---
+++ desafio_fatorial/desafioFatorial.cpp
@@ -0,0 +1,17 @@
+/*
+Dado um inteiro N, determine quanto vale N fatorial (escreve-se N!). O fatorial de um número é o produto de todos os números entre 1 e N, inclusive. Por exemplo, 5! = 5 × 4 × 3 × 2 × 1 = 120.
+*/
+#include <iostream>;
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    cout << "the number you typed is: " << n << endl;
+
+    return 0;
+}
+
+
+
---
+++ desafios_Bomberman.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    
+    int h,w;
+    cin >> h >> w;
+    
+
+
+    return 0;
+}
