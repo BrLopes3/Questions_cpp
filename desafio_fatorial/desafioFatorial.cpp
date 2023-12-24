bc0b52253e85902c6ba0aa927da026ab93c3918d
Bruno Lopes <121700662+BrLopes3@users.noreply.github.com>
2023-12-24 15:10:30 -0500

    commit with update of factorial problem

--- desafio_fatorial/desafioFatorial.cpp
+++ desafio_fatorial/desafioFatorial.cpp
@@ -1,17 +1,30 @@
 /*
 Dado um inteiro N, determine quanto vale N fatorial (escreve-se N!). O fatorial de um número é o produto de todos os números entre 1 e N, inclusive. Por exemplo, 5! = 5 × 4 × 3 × 2 × 1 = 120.
 */
-#include <iostream>;
+#include <iostream>
 using namespace std;
 
+int factorial(int n){
+    if (n ==0 || n==1){
+        return 1;
+    }
+    else{
+        return n*factorial(n-1);
+    }
+}
 int main(){
     int n;
+    cout << "type a number: ";
     cin >> n;
 
-    cout << "the number you typed is: " << n << endl;
+    int fact = factorial(n);
 
+    cout << "factorial of " << n << " is " << fact << endl;
+    
     return 0;
 }
+
+
 
 
 
